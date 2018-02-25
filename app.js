e998ca3c24036e84218b06480a1a69142c6628cd
rrbrink <ryanbrinkerhoff@live.com>
2018-02-25 11:17:32 -0700

    backend major update

--- app.js
+++ app.js
@@ -1,14 +1,62 @@
+
+// var cookieParser = require('cookie-parser');
+// var expressValidator = require('express-validator');
+// var flash = require('connect-flash');
+// var passport = require('passport');
+// var localStrategy = require('passport-local'), Strategy;
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var flash = require('connect-flash');
 var session = require('express-session');
-var passport = require('passport');
-var localStrategy = require('passport-local'), Strategy;
-var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose connect('mongodb://localhost/So-Many-Activities');
-var db = mongoose connection;
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(session({
+  secret: process.env.SESSIONSECRET || config.sessionSecret || "cat",
+  resave: false,
+  saveUninitialized: true
+}));
+
+// this is for auth if you choose to use it
+function userSetup(req, res, next) {
+  if (!req.session.user) {
+    req.session.user = {}
+    req.session.user.loggedIn = false;
+    req.session.user.isAdmin = false;
+  }
+  next()
+}
+
+//using middlewhere acrossed the entire application before any route gets hit.
+app.use(userSetup)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+require('./routes/apiRoutes')(app)
+require('./routes/htmlRoutes')(app)
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/So-Many-Activities';
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// Listen on the port
+app.listen(PORT, function () {
+  console.log("Listening on port: " + PORT);
+});
